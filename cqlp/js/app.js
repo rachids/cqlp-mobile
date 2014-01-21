99a2639a0eef4f6b7edca930c4bb98d395f775ea
rachids <r.zuco@yahoo.fr>
2014-01-21 21:56:34 +0100

    New webservice is up.

--- cqlp/js/app.js
+++ cqlp/js/app.js
@@ -2,17 +2,19 @@ var cqlpApp = angular.module('cqlpApp', []);
 
 cqlpApp.controller('CqlpCtrl', function($scope,$http) {
 	$scope.choix1 = '';
-	$scope.url1 = [];
+	$scope.choix2 = '';
+	$scope.url = '';
 	
 	$scope.compteur = 0;
 
 	$scope.fetch = function() {
-		var url = 'http://www.cestquoilepire.fr/webmaster.php?out=json';
+		var url = 'http://www.cestquoilepire.fr/webmaster.php?out=mobile';
 
 		$http.get(url).success(function(data) {
 
 			$scope.choix1 = data.dilemme1;
-			//$scope.url1 = data.url1;
+			$scope.choix2 = data.dilemme2;
+			$scope.url = data.identifiant;
 
 			$scope.compteur++;
 
